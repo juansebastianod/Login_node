8b8d09d5f0b0e262253e3161dbea94b91c034027
juansebastianod <juansebastianod@ufps.edu.co>
2024-06-09 10:56:26 -0500

    Creacion del token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,39 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import  jwt from 'jsonwebtoken'
+import {tokenAcceso} from '../libs/jwt.js'
 
 export const register = async(req,res)=> {
-
-try {
 const {email,password,username}=req.body
+try {
+
+const passwordHast =await bcrypt.hash(password,10)
 const newUser = new User({
     username,
     email,
-    password
+    password:passwordHast,
 })
 
 const userSave = await newUser.save()
-res.json(userSave);
+
+const token=await tokenAcceso({id:userSave._id})
+
+
+res.cookie('token',token)
+res.json({
+    id:userSave._id,
+    username:userSave.username,
+    email:userSave.email,
+    createdAt:userSave.createdAt,
+    updatedAt:userSave.updatedAt,
+
+})
+// res.json({
+//     
+// });
     
 } catch (error) {
-    console.log(error)
+    return res.status(500).json({message:error.message})
 }
 
 
