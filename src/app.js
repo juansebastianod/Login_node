8eae9d4fdc3ef68c4887d6be334dd09ed8fdf848
juansebastianod <juansebastianod@ufps.edu.co>
2024-06-10 09:42:27 -0500

    Validacion del token

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 import authRouter from './routers/auth.route.js'
 const app=express();
 app.use(morgan('dev'))
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api',authRouter)
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -39,4 +39,75 @@ res.json({
 
 }
 
-export const login = (req,res)=>res.send("login sebas")
+export const login = async(req,res)=> {
+    const {email,password}=req.body
+    try {
+
+    const userFound=  await User.findOne({email})
+    if(!userFound) return res.status(400).json({
+        message:"user not found"
+    })
+    
+    const isMach =await bcrypt.compare(password,userFound.password)
+
+    if(!isMach) return res.status(400).json({
+        message:"password not equial"
+    })
+   
+
+
+    
+    const token=await tokenAcceso({id:userFound._id})
+    
+    
+    res.cookie('token',token)
+    res.json({
+        id:userFound._id,
+        username:userFound.username,
+        email:userFound.email,
+        createdAt:userFound.createdAt,
+        updatedAt:userFound.updatedAt,
+    
+    })
+    // res.json({
+    //     
+    // });
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+    
+    
+    }
+
+export const logout =(req,res)=>{
+try {
+    res.cookie('token',"",{
+        expires: new Date(0)
+    })
+
+    return res.sendStatus(200);
+    
+} catch (error) {
+    console.log(error)
+}
+    }
+
+export const profile =async (req,res)=>{
+
+    const userFound = await User.findById(req.user.id);
+
+    if(!userFound) res.status(401).json({message:"User no encontrado"})
+
+    return res.json({
+        id:userFound._id,
+        username:userFound.username,
+        email:userFound.email,
+        createdAt:userFound.createdAt,
+        updatedAt:userFound.updatedAt,
+
+    })
+
+    console.log(req.user)
+    res.send('profile')
+    }
